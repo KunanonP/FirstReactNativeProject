b2d0ce03fececf49b26eef9025daed2ad76c8314
Brightbite <zindybiteme@gmail.com>
2019-03-06 10:07:20 +1100

    header

--- index.js
+++ index.js
@@ -3,8 +3,16 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Text} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import Header from './src/component/Header';
+import React from 'react';
 
+// init component
+// const App = () => (
+//   <Header />
+// );
+
+// Render on device
 AppRegistry.registerComponent(appName, () => App);
